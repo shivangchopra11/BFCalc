bf9b6ffce5b46b8236efe30dbd9549d89ccc5b84
SHIVANG CHOPRA <shivang@SHIVANGs-MacBook-Pro.local>
2017-12-13 15:15:03 +0530

    Adv features on button click UI

---
+++ app/src/main/java/com/example/shivang/calculator/BasicButtons.java
@@ -0,0 +1,35 @@
+package com.example.shivang.calculator;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by shivang on 13/12/17.
+ */
+
+public class BasicButtons extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public BasicButtons() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.basic_buttons,container,false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/shivang/calculator/FragmentChangeListner.java
@@ -0,0 +1,11 @@
+package com.example.shivang.calculator;
+
+import android.app.Fragment;
+
+/**
+ * Created by shivang on 13/12/17.
+ */
+
+public interface FragmentChangeListner {
+    public void replaceFragment(Fragment fragment);
+}
